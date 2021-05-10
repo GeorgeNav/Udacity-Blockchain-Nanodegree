42cab5e9df7d525d78ddf28adeb706f19eabad0a
GeorgeNav <george.g.navarro@gmail.com>
2021-05-10 13:40:44 -0500

    files up to project 1

---
+++ bitcoin_core/commands/commands.sh
@@ -0,0 +1,10 @@
+getnewaddress # create new receiving address
+
+getnewaddress test # create new receiving address with name test
+
+getnewaddress tb1qca8mkqg0vs8q6jzll4qxv5u52cdsgq4jk4wwrj legacy # get new legacy address
+# output: mgKZwCDwSDLLupC5iRMWwU6zXcUGPw7RdR
+
+listunspent # list unspent outputs (UTXOs)
+
+gettxout "txid" 1 # list transaction with id of txid and vout number 1
---
+++ excercises/block_data_model/simpleChain.js
@@ -0,0 +1,32 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block {
+  constructor(data) {
+    this.hash = ""
+    this.height = 0
+    this.body = data
+    this.time = 0
+    this.previousblockhash = ""
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = []
+    this.addBlock(new Block('First block in the chain - Genesis block'))
+  }
+  addBlock(newBlock) {
+    newBlock.height = this.chain.length
+    newBlock.time = new Date().getTime().toString().slice(0, -3)
+    newBlock.previousblockhash = this.chain.length > 0
+      ? this.chain[this.chain.length - 1].hash
+      : ""
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString()
+    this.chain.push(newBlock)
+  }
+}
+
+const chain = new Blockchain()
+chain.addBlock(new Block('Hello'))
+chain.addBlock(new Block('Hi'))
+console.log(chain)
