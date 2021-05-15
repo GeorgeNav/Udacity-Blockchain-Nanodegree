c2a5e34516dc8047b08b3ef02bf86dfd2d33c9d0
GeorgeNav <george.g.navarro@gmail.com>
2021-05-15 14:59:40 -0500

    final changes

--- excercises/ganache_tx/dapp.js
+++ excercises/ganache_tx/dapp.js
@@ -4,22 +4,26 @@ const { Transaction } = require('ethereumjs-tx')
 const ganacheURL = 'http://127.0.0.1:7545'
 const web3 = new Web3(ganacheURL)
 
+const getEOCAccountInfo = async (EOS, text) => {
+    EOS.balance = await web3.eth.getBalance(EOS.address)
+    EOS.nonce = await web3.eth.getTransactionCount(EOS.address)
+    console.log(text, EOS)
+}
+
 web3.eth.getAccounts()
   .then(async (accounts) => {
-    const sender = {address: accounts[0]}
-    const receiver = {address: accounts[1]}
+    const sender = {
+      address: accounts[0],
+      privateKey: new Buffer.from('9954763fcb5b3ee983680c90985cac4dcb8f71ceef36ae8410eba8bb72f4ebd2', 'hex'),
+    }
+    const receiver = {
+      address: accounts[1],
+      privateKey: new Buffer.from('c4469ae781f6836ef3de6ba9a9642964e3c04afb4af110ef72e7a073bcb680e8', 'hex'),
+    }
 
-    await web3.eth.getBalance(sender.address).then((balance) => {
-      console.log(`${sender.address} (before): ${balance}`)
-      sender.balance = balance
-    })
-    sender.nonce = await web3.eth.getTransactionCount(sender.address)
-
-    await web3.eth.getBalance(receiver.address).then((balance) => {
-      console.log(`${receiver.address} (before): ${balance}`)
-      receiver.balance = balance
-    })
-    receiver.nonce = await web3.eth.getTransactionCount(receiver.address)
+    console.log('BEFORE')
+    await getEOCAccountInfo(sender, 'sender (before)')
+    await getEOCAccountInfo(receiver, 'receiver (before)')
 
     const rawTransaction = {
       nonce: sender.nonce, // TODO: this must be incremented after every transaction
@@ -30,20 +34,12 @@ web3.eth.getAccounts()
       data: "0x", // NOTE: left blank since it's to a EOA (would have something if sent to CA)
     }
 
-    sender.privateKey = new Buffer.from('9954763fcb5b3ee983680c90985cac4dcb8f71ceef36ae8410eba8bb72f4ebd2', 'hex')
     const transaction = new Transaction(rawTransaction)
     transaction.sign(sender.privateKey)
     const serializedTransaction = transaction.serialize()
     await web3.eth.sendSignedTransaction(serializedTransaction)
 
-    await web3.eth.getBalance(sender.address).then((balance) => {
-      console.log(`${sender.address} (after): ${balance}`)
-      sender.balance = balance
-    })
-
-    await web3.eth.getBalance(receiver.address).then((balance) => {
-      console.log(`${receiver.address} (after): ${balance}`)
-      receiver.balance = balance
-    })
+    await getEOCAccountInfo(sender, 'sender (after)')
+    await getEOCAccountInfo(receiver, 'receiver (after)')
   })
   .catch(console.log)
